add8d874482eaba03624491028aa1b73298464c7
meaton212 <matt.eaton212@gmail.com>
2025-07-31 16:37:48 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,5 +52,5 @@ if time_to_insert:
 
 
 import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+smoothiefroot_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 st.text(smoothiefroot_response)
